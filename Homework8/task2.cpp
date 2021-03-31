4abb2c250bc070660d71a817fa43be028046eb31
MaslennikovDmitriy <Maslennikov.DV@phystech.edu>
2021-03-31 20:23:00 +0300

    Task 2 completed

--- Homework8/task2.cpp
+++ Homework8/task2.cpp
@@ -34,11 +34,53 @@ void Rand_String_Generator(std::string& main_string)
     }
 }
 
+int Checker(char a, char b)
+{
+    if (a == b)
+    {
+        return 1;
+    }
+    else return 0;
+}
+
+void Substring_Searcher(std::string main_string, std::string substring, std::vector<size_t>& Iterators)
+{
+    int count = 0;
+    for (size_t i = 0; i < main_string.size(); i++)
+    {
+        for (size_t j = 0; j < substring.size(); j++)
+        {
+            if (i + j < main_string.size())
+            {
+                count += Checker(main_string[i + j], substring[j]);
+            }
+        }
+        if (count == substring.size())
+        {
+            Iterators.push_back(i);
+        }
+        count = 0;
+    }
+}
+
 int main()
 {
-    std::string main_string;
+    std::string main_string, substring;
     Rand_String_Generator(main_string);
     std::cout << main_string;
+    std::cout << std::endl;
+    std::cout << "Insert the fragment you are looking for: ";
+    std::cin >> substring;
+    std::vector<size_t> Iterators;
+    Substring_Searcher(main_string, substring, Iterators);
+    for (size_t i = 0; i < Iterators.size(); i++)
+    {
+        std::cout << "The beginning of " << i + 1 << " substring: " << Iterators[i] << std::endl;
+    }
+    if (Iterators.size() == 0)
+    {
+        std::cout << "No matches" << std::endl;
+    }
     system("pause");
     return EXIT_SUCCESS;
 }
