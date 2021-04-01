e9615687e511b8d9f2ceb21916d35d2ea34c1571
MaslennikovDmitriy <Maslennikov.DV@phystech.edu>
2021-04-01 14:21:27 +0300

    Task 4: added test of the threadsafe queue & boost queue

--- Homework8/task4.cpp
+++ Homework8/task4.cpp
@@ -9,6 +9,8 @@
 #include <thread>
 #include <vector>
 #include <functional>
+#include <condition_variable>
+#include <queue>
 
 template < typename T >
 class Threadsafe_Stack
@@ -77,6 +79,101 @@ private:
 };
 
 
+template < typename T >
+class Threadsafe_Queue
+{
+public:
+
+	Threadsafe_Queue() = default;
+
+	Threadsafe_Queue(const Threadsafe_Queue& other)
+	{
+		std::lock_guard <std::mutex> lock(other.m_mutex);
+		m_queue = other.m_queue;
+	}
+
+	Threadsafe_Queue& operator=(const Threadsafe_Queue& other)
+	{
+		std::lock_guard <std::mutex> lock(other.m_mutex);
+		m_queue = other.m_queue;
+	}
+
+public:
+
+	void push(T value)
+	{
+		std::lock_guard < std::mutex > lock(m_mutex);
+		m_queue.push(value);
+		m_condition_variable.notify_one();
+	}
+
+	void wait_and_pop(T& value)
+	{
+		std::unique_lock < std::mutex > lock(m_mutex);
+
+		m_condition_variable.wait(lock, [this] {return !m_queue.empty(); });
+		value = m_queue.front();
+		m_queue.pop();
+	}
+
+	std::shared_ptr < T > wait_and_pop()
+	{
+		std::unique_lock < std::mutex > lock(m_mutex);
+
+		m_condition_variable.wait(lock, [this] {return !m_queue.empty(); });
+		auto result = std::make_shared < T >(m_queue.front());
+		m_queue.pop();
+
+		return result;
+	}
+
+	bool try_pop(T& value)
+	{
+		std::lock_guard < std::mutex > lock(m_mutex);
+
+		if (m_queue.empty())
+		{
+			return false;
+		}
+
+		value = m_queue.front();
+		m_queue.pop();
+
+		return true;
+	}
+
+	std::shared_ptr < T > try_pop()
+	{
+		std::lock_guard < std::mutex > lock(m_mutex);
+
+		if (m_queue.empty())
+		{
+			return std::shared_ptr < T >();
+		}
+
+		auto result = std::make_shared < T >(m_queue.front());
+		m_queue.pop();
+
+		return result;
+	}
+
+	bool empty() const
+	{
+		std::lock_guard < std::mutex > lock(m_mutex);
+		return m_queue.empty();
+	}
+
+private:
+
+	std::queue <T> m_queue;
+	std::condition_variable m_condition_variable;
+
+private:
+
+	mutable std::mutex m_mutex;
+};
+
+
 
 template <typename T>
 void Threadsafe_Stack_Adding(Threadsafe_Stack <T>& stack, const size_t M)
@@ -97,8 +194,6 @@ void Threadsafe_Stack_Removing(Threadsafe_Stack <T>& stack, const size_t M)
 }
 
 
-
-
 template <typename T>
 void Boost_Stack_Adding(boost::lockfree::stack<T>& stack, const size_t M)
 {
@@ -117,8 +212,47 @@ void Boost_Stack_Removing(boost::lockfree::stack<T>& stack, const size_t M)
 	}
 }
 
+
+
 template <typename T>
-void Tester(Threadsafe_Stack <T>& stack1, boost::lockfree::stack<T>& stack2, const size_t N, const size_t M)
+void Threadsafe_Queue_Adding(Threadsafe_Queue <T>& queue, const size_t M)
+{
+	for (int i = 0; i < M; i++)
+	{
+		queue.push(i);
+	}
+}
+
+template <typename T>
+void Threadsafe_Queue_Removing(Threadsafe_Queue <T>& queue, const size_t M)
+{
+	for (int i = 0; i < M; i++)
+	{
+		queue.try_pop(i);
+	}
+}
+
+
+template <typename T>
+void Boost_Queue_Adding(boost::lockfree::queue<T>& queue, const size_t M)
+{
+	for (int i = 0; i < M; i++)
+	{
+		queue.push(i);
+	}
+}
+
+template <typename T>
+void Boost_Queue_Removing(boost::lockfree::queue<T>& queue, const size_t M)
+{
+	for (int i = 0; i < M; i++)
+	{
+		queue.pop(i);
+	}
+}
+
+template <typename T>
+void Tester(Threadsafe_Stack <T>& stack1, boost::lockfree::stack<T>& stack2, Threadsafe_Queue <T>& queue1, boost::lockfree::queue<T>& queue2, const int N, const int M)
 {
 	std::vector<std::thread> Threads(2 * N);
 	for (size_t i = 0; i < N; i++)
@@ -135,7 +269,6 @@ void Tester(Threadsafe_Stack <T>& stack1, boost::lockfree::stack<T>& stack2, con
 	auto elapsed1 = std::chrono::duration_cast<std::chrono::microseconds>(end1 - now1);
 	std::cout << "Threadsafe_Stack processing time(microseconds): " << elapsed1.count() << std::endl;
 
-
 	for (size_t i = 0; i < N; i++)
 	{
 		Threads[i] = std::thread(Boost_Stack_Adding <T>, std::ref(stack2), M);
@@ -149,6 +282,37 @@ void Tester(Threadsafe_Stack <T>& stack1, boost::lockfree::stack<T>& stack2, con
 	auto end2 = std::chrono::system_clock::now();
 	auto elapsed2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - now2);
 	std::cout << "Boost_Stack processing time(microseconds): " << elapsed2.count() << std::endl;
+
+
+
+	for (size_t i = 0; i < N; i++)
+	{
+		Threads[i] = std::thread(Threadsafe_Queue_Adding <T>, std::ref(queue1), M);
+	}
+	for (size_t i = N; i < 2 * N; i++)
+	{
+		Threads[i] = std::thread(Threadsafe_Queue_Removing <T>, std::ref(queue1), M);
+	}
+	auto now3 = std::chrono::system_clock::now();
+	std::for_each(Threads.begin(), Threads.end(), std::mem_fn(&std::thread::join));
+	auto end3 = std::chrono::system_clock::now();
+	auto elapsed3 = std::chrono::duration_cast<std::chrono::microseconds>(end3 - now3);
+	std::cout << "Threadsafe_Queue processing time(microseconds): " << elapsed3.count() << std::endl;
+
+
+	for (size_t i = 0; i < N; i++)
+	{
+		Threads[i] = std::thread(Boost_Queue_Adding <T>, std::ref(queue2), M);
+	}
+	for (size_t i = N; i < 2 * N; i++)
+	{
+		Threads[i] = std::thread(Boost_Queue_Removing <T>, std::ref(queue2), M);
+	}
+	auto now4 = std::chrono::system_clock::now();
+	std::for_each(Threads.begin(), Threads.end(), std::mem_fn(&std::thread::join));
+	auto end4 = std::chrono::system_clock::now();
+	auto elapsed4 = std::chrono::duration_cast<std::chrono::microseconds>(end4 - now4);
+	std::cout << "Boost_Queue processing time(microseconds): " << elapsed4.count() << std::endl;
 }
 
 
@@ -157,13 +321,15 @@ int main()
 {
 	Threadsafe_Stack <int> stack1;
 	boost::lockfree::stack<int> stack2(1);
+	Threadsafe_Queue <int> queue1;
+	boost::lockfree::queue<int> queue2(1);
 	int N = 0, M = 0;
 	std::cout << "Enter the number of threads: ";
 	std::cin >> N;
 	std::cout << "Enter the number of elements: ";
 	std::cin >> M;
 	std::cout << std::endl;
-	Tester(stack1, stack2, N, M);
+	Tester(stack1, stack2, queue1, queue2, N, M);
 	system("pause");
 	return EXIT_SUCCESS;
 }
