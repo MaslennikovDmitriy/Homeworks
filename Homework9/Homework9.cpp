c0fb9fd202c3cd734e15b316c34c1397dfe6965c
MaslennikovDmitriy <Maslennikov.DV@phystech.edu>
2021-04-08 22:46:33 +0300

    Added string_vector in task1

--- Homework9/Homework9.cpp
+++ Homework9/Homework9.cpp
@@ -1,20 +1,62 @@
-﻿// Homework9.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+﻿#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/container/scoped_allocator.hpp>
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/containers/map.hpp>
+#include <boost/interprocess/containers/vector.hpp>
+#include <boost/interprocess/containers/string.hpp>
 #include <iostream>
+
+using namespace boost::interprocess;
+
+typedef managed_shared_memory::segment_manager segment_manager_t;
+typedef boost::container::scoped_allocator_adaptor<allocator<void, segment_manager_t> > void_allocator;
+typedef void_allocator::rebind<std::string>::other      string_allocator;
+typedef vector<std::string, string_allocator>       string_vector;
+
+
+class complex_data
+{
+public:
+    string_vector string_vector;
+
+    typedef void_allocator allocator_type;
+
+    complex_data(complex_data const& other, const allocator_type& void_alloc)
+        : string_vector(other.string_vector, void_alloc)
+    {}
+    complex_data(const allocator_type& void_alloc)
+        : string_vector(void_alloc)
+    {}
+
+};
+
+typedef void_allocator::rebind<complex_data>::other    complex_data_allocator;
+typedef vector<complex_data, complex_data_allocator>   complex_data_vector;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    struct shared_memory_remove
+    {
+        shared_memory_remove() { shared_memory_object::remove("MySharedMemory"); }
+        ~shared_memory_remove() { shared_memory_object::remove("MySharedMemory"); }
+    } remover;
+
+    managed_shared_memory segment(open_or_create, "MySharedMemory", 65536);
+    void_allocator alloc_inst(segment.get_segment_manager());
+    complex_data* complex_data0_ = segment.construct<complex_data>("MyComplexData")(alloc_inst);
+
+    std::string message;
+    size_t counter = 0;
+    while (message != "end")
+    {
+        std::cin >> message;
+        complex_data0_->string_vector.push_back(message);
+        counter++;
+    }
+    std::cout << std::endl;
+    for (size_t i = 0; i < counter - 1; i++)
+    {
+        std::cout << complex_data0_->string_vector[i] << std::endl;
+    }
+    return EXIT_SUCCESS;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
